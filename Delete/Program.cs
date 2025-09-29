e141e32e0c1c958376e781c432053c0081ef2a85
Thermopylai <lauri.tikkanen@gmail.com>
2025-09-29 16:51:25 +0300

    Add project files.

---
+++ Delete/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Delete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\nMissing arguments.\n");
+            }
+            else if (args.Length == 1 && File.Exists(args[0]))
+            {
+                File.Delete(args[0]);
+                Console.WriteLine($"\nFile '{args[0]}' deleted.\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nFile '{args[0]}' doesn't exist or too many arguments.\n");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
